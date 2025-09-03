f481353b3d4145dc3b97993cfc39b35c80bb46e2
GauharV <gveeravalli@my.waketech.edu>
2025-09-03 09:57:51 -0400

    Add files via upload

---
+++ Currency.java
@@ -0,0 +1,14 @@
+import Scanner;
+
+public class Currency {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static String getInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public static int getIntInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextInt();
+    }
